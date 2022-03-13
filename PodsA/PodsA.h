e8533f4a7eef8390dd0160c633c7e42a936cb7b5
Azuer <azuer@outlook.com>
2022-03-13 20:21:44 +0800

    Initial Commit

---
+++ PodsA/PodsA.h
@@ -0,0 +1,18 @@
+//
+//  PodsA.h
+//  PodsA
+//
+//  Created by Azuer on 2022/3/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PodsA.
+FOUNDATION_EXPORT double PodsAVersionNumber;
+
+//! Project version string for PodsA.
+FOUNDATION_EXPORT const unsigned char PodsAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodsA/PublicHeader.h>
+
+
